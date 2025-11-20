63bb4f4a7472d8b65e4f5a8382e316920dc6c811
Nikitos <krasilnikovnikita11@gmail.com>
2025-11-20 19:55:58 +0300

    RTK slice по добавлению борда и взятию id из task

--- src/store/Slice/todosSlice.ts
+++ src/store/Slice/todosSlice.ts
@@ -1,45 +1,43 @@
-import {createSlice, type PayloadAction} from '@reduxjs/toolkit'
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
 
 
 type Todo = {
-    id: string
-    title: string
-    description?: string
-    done: boolean
-    board: []
+  id: string
+  title: string
+  description?: string
+  done: boolean
 }
 
 type TodosState = {
-    items: Todo[]
+  items: Todo[]
 }
-const initialState:TodosState = {
-    items: [],
+const initialState: TodosState = {
+  items: [],
 }
 
 const todosSlice = createSlice({
-    name: 'tasks',
-    initialState,
-    reducers: {
-        addTodo: (state, action: PayloadAction<{ title: string, description?: string }>) => {
-            const newTodo: Todo = {
-                id: Date.now().toString(),
-                title: action.payload.title,
-                description: action.payload.description ?? '',
-                done: false,
-                board: boardsSlice
-            }
-            state.items.push(newTodo)
-        },
-        deleteTodo: (state, action: PayloadAction<{ id: string }>) => {
-            const removeItem = state.items.filter(i => i.id !== action.payload.id)
-            return {...state, items: removeItem}
-        },
-        editTodo: (state, action: PayloadAction<{ id: string }>) => {
-            const editItem = state.items.map(i => i.id === action.payload.id)
-            return {...state, items: editItem}
-        }
-    }
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<{ title: string, description?: string }>) => {
+      const newTodo: Todo = {
+        id: Date.now().toString(),
+        title: action.payload.title,
+        description: action.payload.description ?? '',
+        done: false,
+      }
+      state.items.push(newTodo)
+    },
+    deleteTodo: (state, action: PayloadAction<{ id: string }>) => {
+      const removeItem = state.items.filter(i => i.id !== action.payload.id)
+      return { ...state, items: removeItem }
+    },
+    editTodo: (state, action: PayloadAction<{ id: string }>) => {
+      const editItem = state.items.map(i => i.id === action.payload.id)
+      return { ...state, items: editItem }
+    },
+  },
 })
 
 export default todosSlice.reducer
-export const {addTodo, deleteTodo, editTodo} = todosSlice.actions
+export const { addTodo, deleteTodo, editTodo } = todosSlice.actions
---
+++ src/store/board/boardsSlice.ts
@@ -0,0 +1,62 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { data } from 'autoprefixer'
+
+type BoardTask = {
+  id: string
+  title: string
+  description: string
+  todoId: string
+  done: boolean
+}
+
+type ColumnId = 'todo' | 'inProgress' | 'done'
+
+type Board = {
+  columns: Record<ColumnId, BoardTask[]>
+}
+
+type BoardState = {
+  byTodoId: Record<string, Board>
+}
+
+const initialState: BoardState = {
+  byTodoId: {},
+}
+const boardSlice = createSlice({
+  name: 'Board',
+  initialState,
+  reducers: {
+    addBoard: (state, action: PayloadAction<{
+      title: string
+      description: string
+      todoId: string
+      columnId: ColumnId
+    }>) => {
+      const { todoId, columnId, title, description } = action.payload
+
+      let board = state.byTodoId[todoId]
+      if (!board) {
+        board = {
+          columns: {
+            todo: [],
+            inProgress: [],
+            done: [],
+          },
+        }
+        state.byTodoId[todoId] = board
+      }
+
+
+      const newTask: BoardTask = {
+        id: Date.now().toString(),
+        title,
+        description,
+        todoId,
+        done: false,
+      }
+
+      board.columns[columnId].push(newTask)
+    },
+
+  },
+})
